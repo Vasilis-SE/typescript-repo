814975a6421125c93581da3163361d2966e97ce3
Vasilis CodeX <vasilis.tris@gmail.com>
2021-03-25 13:36:57 +0200

    gulp execute multiple tasks on default & watch changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,6 @@ const ts = require("gulp-typescript");
 const tsProject = ts.createProject("tsconfig.json");
 const imagemin = require('gulp-imagemin');
 
-// Print message
-gulp.task('message', function() {
-    console.log('Gulp is running...');
-});
-
 // Copy all html files
 gulp.task('copy-html', function() {
     return gulp.src('src/**/*.html').pipe(gulp.dest("dist"));
@@ -20,7 +15,18 @@ gulp.task('image-optimizer', function() {
         .pipe(gulp.dest("dist/assets/images"));
 });
 
+// Compile ts to js files
+gulp.task('compile-typescript', function() {
+    return tsProject.src()
+        .pipe(tsProject()).js
+        .pipe(gulp.dest("dist"));
+});
+
 // Default gulp task
-gulp.task("default", function () {
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+gulp.task("default", gulp.series('copy-html', 'image-optimizer', 'compile-typescript'));
+
+gulp.task("watch", function() {
+    gulp.watch("src/**/*.ts", ["compile-typescript"]);
+    gulp.watch("src/assets/images/*", ["image-optimizer"]);
+    gulp.watch("src/**/*.html", ["copy-html"]);
 });
