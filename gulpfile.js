7e7093389d43f3b291dbc2f63ad96aaae50b8b17
Vasilis CodeX <vasilis.tris@gmail.com>
2021-03-25 13:16:17 +0200

    New gulp tasks and image optimizer with gulp-imagemin package

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+const imagemin = require('gulp-imagemin');
 
+// Print message
+gulp.task('message', function() {
+    console.log('Gulp is running...');
+});
+
+// Copy all html files
+gulp.task('copy-html', function() {
+    return gulp.src('src/**/*.html').pipe(gulp.dest("dist"));
+});
+
+// Optimizing images
+gulp.task('image-optimizer', function() {
+    return gulp.src('src/assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/assets/images"));
+});
+
+// Default gulp task
 gulp.task("default", function () {
     return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
 });
